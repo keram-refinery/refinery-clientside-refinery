7e90848f40e121c459927beedd3a78bac20606e0
Marek Labos <keraml@gmail.com>
2013-08-24 19:52:22 +0200

    ajax response stub helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,31 @@
-function uiSelect(item) {
-  $(item).addClass("ui-selected")
-    .trigger('selectableselected', {
-      selected: item
-    });
+function uiSelect (item) {
+    $(item).addClass("ui-selected")
+        .trigger('selectableselected', {
+            selected: item
+        });
 }
+
+function errorResponse (data) {
+    var d;
+
+    data = data || {};
+    d = $.Deferred();
+
+    d.reject(data, 404, 'something went wrong');
+    return d.promise();
+};
+
+function okResponse (data) {
+    var d;
+
+    data = data || {};
+    d = $.Deferred();
+
+    d.resolve(data, 'success', {
+      getResponseHeader: function (args) {
+        return false;
+      }
+    });
+
+    return d.promise();
+};
